73b16658d6ac3c318bc2654603907d3a3be9918f
Ashmit Tyagi <tyagiashmit4@gmail.com>
2025-07-22 15:03:42 +0530

    xcvb

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,11 +1,9 @@
  
 import HeroArea from './HeroArea'
-import BrandArea from './BrandArea'
 import AboutArea from './AboutArea'
 import ServiceArea from './ServiceArea' 
 import PortfolioArea from './PortfolioArea'
 import TestimonoalArea from './TestimonoalArea'
-// import BlogArea from './BlogArea'
 import ContactArea from './ContactArea' 
 import HeaderOne from '../../layouts/headers/HeaderOne'
 import FooterOne from '../../layouts/footers/FooterOne'
@@ -13,7 +11,7 @@ import ScrollTop from '../common/ScrollTop'
 import CustomCursor from '../common/CustomCursor'
 import ScrollToTop from '../common/ScrollToTop'
 
-export default function Home() {
+export default function Home() { 
   return (
     <>
       <HeaderOne />
@@ -21,12 +19,10 @@ export default function Home() {
         <div id="smooth-content">
           <main>
             <HeroArea />
-            <BrandArea />
             <AboutArea />
             <ServiceArea />
             <PortfolioArea />
             <TestimonoalArea />
-            {/* <BlogArea /> */}
             <ContactArea />
           </main>
           <FooterOne />
