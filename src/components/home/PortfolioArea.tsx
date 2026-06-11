722db2c5f64196eafd6b72a99cf7bc606f414ef9
Ashmit Tyagi <tyagiashmit4@gmail.com>
2026-06-11 17:05:37 +0530

    Code Updated

--- src/components/home/PortfolioArea.tsx
+++ src/components/home/PortfolioArea.tsx
@@ -17,9 +17,8 @@ const portfolio_data: DataType[] = [
   { id: 1, col: "6", image: "/assets/images/projects/amarujala.jpeg", title: "Amarujala News App", category: "App Development", link: "https://play.google.com/store/apps/details?id=com.org.AmarUjala.news&pcampaignid=web_share" },
   { id: 2, col: "6", image: "/assets/images/projects/myjyotish.jpeg", title: "Myjyotish Astrologer App", category: "App Development", link: "https://play.google.com/store/apps/details?id=com.amarujala.myjyotish&pcampaignid=web_share" },
   { id: 3, col: "4", image: "/assets/images/projects/thebonus.jpeg", title: "TheBonus app", category: "App Development", link: "https://play.google.com/store/apps/details?id=com.org.amarujala.thebonus&pcampaignid=web_share" },
-  { id: 4, col: "4", image: "/assets/images/Lens.png", title: "LensCorp Replica", category: "Web Interface", link: "https://lenscorp-replica.vercel.app/" },
-  { id: 6, col: "6", image: "/assets/images/Dashbaord.png", title: "React Dashboard", category: "Web Interface", link: "https://react-project-assignment-eight.vercel.app/" },
-  { id: 7, col: "6", image: "/assets/images/editor.png", title: "Code Editor", category: "Web Interface", link: "https://code-editor-pied-tau.vercel.app/" },
+  { id: 8, col: "6", image: "/assets/images/brainquest.png", title: "BrainQuest", category: "Web Application", link: "https://aiquiz.ashmittyagi.space/" },
+  { id: 9, col: "6", image: "/assets/images/localfix.png", title: "LocalFix", category: "Web Application", link: "https://localfix.ashmittyagi.space/" },
 ];
 
 const ProjectCard = ({ item, index, onClick }: { item: DataType, index: number, onClick: () => void }) => {
